a3017ff1273371203f417dbb2bceb88aba96e218
chivicoko <victor.c.okoye@gmail.com>
2024-01-17 04:13:53 +0000

    add more modularization...

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,34 +1,26 @@
 
 import React, { useState } from 'react';
 import styles from '../styles/style.module.css';
-import { useGlobContext } from '../context/context';
 import Items from './Items';
+import TaskCategories from './TaskCategories';
+import { useFormContext } from '../context/formContext';
 
 const Form = () => {
-  const { handleChange, handleSubmit, task } = useGlobContext();
-  const [selectedCategory, setSelectedCategory] = useState('');
-  const [newTaskCategory, setNewTaskCategory] = useState('');
-  const categories = ['Family', 'Work', 'Personal'];
-
-  const handleCategorySelect = (category) => {
-    setSelectedCategory(category);
-    setNewTaskCategory(category);
-  };
+  const { handleChange, handleSubmit, task, handleCategorySelect, selectedCategory, newTaskCategory, setNewTaskCategory } = useFormContext();
 
   return (
     <div className={styles.taskform}>
       <div className={styles.container} id="parentDiv">
         <form onSubmit={(e) => handleSubmit(e, newTaskCategory)}>
           <input className={styles.taskinput} value={task} onChange={handleChange} type="text" placeholder="Enter a task here" />
+          
           <label>
             <span>Category:</span>
             <select className={styles.categoryDropdown} value={newTaskCategory} onChange={(e) => setNewTaskCategory(e.target.value)}>
-              <option value="" disabled>Select a category</option>
-              {categories.map((category) => (
-                <option key={category} value={category}> {category} </option>
-              ))}
+                <option value="" disabled>Select a category</option>
+                <TaskCategories />
             </select>
-          </label>
+        </label>
           <button className={styles.taskbutton} type="submit">
             Add
           </button>
